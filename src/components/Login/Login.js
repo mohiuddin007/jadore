55412397df1dc572d679f590e9134f032eaa3fa1
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2021-01-03 01:28:22 +0600

    login page design almost complete

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,10 +3,10 @@ import './Login.css';
 
 const Login = () => {
     return (
-        <div className="backgroundStyle">
+        <div className="backgroundStyle text-white">
             <div className="row justify-content-center">
                 <div className="col-8 col-sm-8 col-md-5 col-lg-5 col-xl-5">
-                    <h4 className="text-center text-white mb-4">Login or SingUp</h4>
+                    <h4 className="text-center mb-4">Login or SingUp</h4>
                     <form>
                         <div className="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email address</label>
@@ -17,13 +17,19 @@ const Login = () => {
                             <input type="password" className="form-control" placeholder="Write a password" />
                         </div>
                         <div className="row justify-content-end">
-                             <div className="col-md-4">
-                                 <p className="text-danger">Forgot password?</p>
-                             </div>
+                            <div className="col-md-4">
+                                <p className="text-danger">Forgot password?</p>
+                            </div>
                         </div>
                         <div className="d-grid gap-2">
-                            <button className="btn btn-danger rounded-pill" type="button">Login</button>
+                            <button className="btn btn-danger rounded-pill py-3" type="button"><b>Login</b> </button>
                         </div>
+                        <p className="text-center mt-3">Or via social media</p>
+                        <div className="row d-flex justify-content-center mt-3">
+                            <div className="col-md-2"><img src="https://i.ibb.co/Nj1NRdn/download.png" className="logo" alt="" /> </div>
+                            <div className="col-md-2"> <img src="https://i.ibb.co/r75nsLd/google.png" className="logo" alt="" /></div>
+                        </div>
+                        <p className="text-center mt-3">Dont have an account? <span className="text-danger">Sign Up</span></p>
                     </form>
                 </div>
             </div>
