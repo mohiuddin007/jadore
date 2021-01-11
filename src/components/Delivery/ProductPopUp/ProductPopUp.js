03dafc875c1a4e6e5869f1853aab7c1398bda539
unknown <mohiuddinmazumder94@gmail.com>
2021-01-11 11:39:12 +0600

    product pop up

---
+++ src/components/Delivery/ProductPopUp/ProductPopUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProductPopUp.css';
+
+const ProductPopUp = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProductPopUp;
