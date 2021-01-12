2d68f25e010f577614e0dfc99beaf2cf4edbfc9b
anikkhann <anikkhann46@gmail.com>
2021-01-12 18:46:16 +0600

    design complete and radio button half done

--- src/components/Delivery/ProductPopUp/ProductPopUp.js
+++ src/components/Delivery/ProductPopUp/ProductPopUp.js
@@ -1,198 +1,290 @@
-import React from 'react';
-import './ProductPopUp.css';
-import '../Cart/YourCart.css';
-import burger from '../../../images/HomePageResources/Burger2 1.png';
+import React, { useState } from "react";
+import "./ProductPopUp.css";
+import "../Cart/YourCart.css";
+import burger from "../../../images/HomePageResources/Burger2 1.png";
 import ReactStars from "react-rating-stars-component";
-import { Form } from 'react-bootstrap';
+import { Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const ProductPopUp = () => {
-    const rating = {
-        size: 20,
-        value: 5,
-        activeColor: "red",
-        edit: true
-    };
-    return (
-        <form >
-            <div className='container' style={{backgroundColor:'#434343'}}>
-            <div className="row pb-5">
-                    <div className="col-md-8">
-                        <div className="row mt-3 yourCart">
-                            <div className="col-4 bg-warning cartImgBorder">
-                                <img src={burger} className="img-fluid mx-auto yourCartImg" alt="" />
-                            </div>
-                            <div className="col-8 p-3 yourCartBody">
-                                <div className="row justify-content-between ml-4">
-                                    <div className="col-7 text-light"><h4>Big boss Burger</h4></div>
-                                    <div className="col-5"><ReactStars {...rating} /></div>
-                                </div>
-                                <p className="mt-4 ml-5 text-light">$450/piece</p>
-                                <div className="row mt-5 justify-content-between ml-5">
-                                    <div className="col-7">
-                                        <div className="row text-center">
-                                            <div className="col-md-3 border border-secondary text-light">-</div>
-                                            <div className="col-md-3 border border-secondary text-light">1</div>
-                                            <div className="col-md-3 border border-secondary text-light" >+</div>
-                                        </div>
+  const rating = {
+    size: 20,
+    value: 5,
+    activeColor: "red",
+    edit: true,
+  };
+  const { register, handleSubmit, watch, errors } = useForm();
+  const [dropdown, setDropdown] = useState();
+  const [radio, setRadio] = useState();
+  const [multipleRadio, setMultipleRadio] = useState([]);
+  const [text, setText] = useState({
+    textArea: "",
+  });
+  const { textArea } = text;
+  const onTextChange = (e) => {
+    setText({ ...text, [e.target.name]: e.target.value });
+    // console.log(e.target.value);
+  };
 
-                                    </div>
-                                    <div className="col-5"><h4 className="text-center text-danger">$129</h4></div>
-                                </div>
+  //   console.log(radio);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // const getState = {...multipleRadio}
+    setMultipleRadio([{ ...multipleRadio, [name]: value }]);
+    console.log(value);
+  };
 
-                            </div>
-                        </div>
-                        <br/>
-                    {/* select variation work */}
-                    <h2 className="text-light">Select Variation</h2>
-                    <br/>
-                    <div className="row justify-content-between">
-                        
-                        
-                       
-                        <div className="form-check col-5">
-                            <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"/>
-                            <label className="form-check-label text-light" for="exampleRadios2">
-                             Large Size (24'')
-                            </label>
-                          
-                        </div>
-                        <div className="col-4 text-right">
-                            <label className="text-light">+USD 7.00</label>
-                        </div>
-                        
-                    </div>
-                    <br/>
-
-                    <div className="row justify-content-between">
-                        
-                        
-                        
-                        <div className="form-check col-5">
-                            <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"/>
-                            <label className="form-check-label text-light" for="exampleRadios2">
-                             Medium Size (12'')
-                            </label>
-                          
-                        </div>
-                        <div className="col-4 text-right">
-                            <label className="text-light">+USD 7.00</label>
-                        </div>
-                    </div>
-                    {/* abar add korbo */}
-                    <br/>
-
-<div className="row justify-content-between">
-    
-    
-    
-    <div className="form-check col-5">
-        <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"/>
-        <label className="form-check-label text-light" for="exampleRadios2">
-         Large Size (06'')
-        </label>
-      
-    </div>
-    <div className="col-4 text-right">
-        <label className="text-light">+USD 7.00</label>
-    </div>
-   
-</div>
-<br/>
-
-{/*You can add extra  */}
-<h2 className="text-light">You can add extra</h2>
-<br/>
-<div className="row justify-content-between">
-    
-    
-
-    <div className="form-check col-5">
-        <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"/>
-        <label className="form-check-label text-light" for="exampleRadios2">
-         Extra salad
-        </label>
-      
-    </div>
-    <div className="col-4 text-right"> 
-        <label className="text-light">+USD 7.00</label>
-    </div>
-    
-</div>
-<br/>
-
-<div className="row justify-content-between">
-    
-    
-   
-    <div className="form-check col-5">
-        <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"/>
-        <label className="form-check-label text-light" for="exampleRadios2">
-         Extra onion
-        </label>
-      
-    </div>
-    <div className="col-4 text-right">
-        <label className="text-light">+USD 7.00</label>
-    </div>
-    
-</div>
-<br/>
-
-<div className="row justify-content-between">
-    
-    
-    
-    <div className="form-check col-5">
-        <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"/>
-        <label className="form-check-label text-light" for="exampleRadios2">
-         Drink
-        </label>
-      
-    </div>
-    <div  className="col-4 text-right">
-        <label className="text-light">+USD 7.00</label>
-    </div>
-    
-</div>
-<br/>
-{/* special instruction */}
-<form>
-    <div class="form-group">
-        <h2 for="exampleFormControlTextarea1" className="text-light">Special Instruction</h2>
-        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="write here" rows="3"></textarea>
-    </div>
-</form>
-<br/>
-<Form>
-<Form.Group controlId="exampleForm.SelectCustomSizeSm">
-    <h2 className="text-light">If this product is not available</h2>
-    <Form.Control as="select" size="sm" custom>
-      <option>Select one</option>
-      <option></option>
-      <option></option>
-      <option></option>
-      <option></option>
-    </Form.Control>
-  </Form.Group>
-</Form>
-<br/>
-<button className="btn btn-success btn-block rounded-pill mt-2 checkoutBtn">Add to cart</button>
-
-                    </div>
-               
-
-                    </div>
-                 
-                   
+  return (
+    <form>
+      <div className="container">
+        <div className="row pb-5">
+          <div className="col-md-8 mx-auto p-4 popupColor">
+            <div className="row mt-3 yourCart">
+              <div className="col-4 bg-warning cartImgBorder">
+                <img
+                  src={burger}
+                  className="img-fluid mx-auto yourCartImg"
+                  alt=""
+                />
+              </div>
+              <div className="col-8 p-3 yourCartBody">
+                <div className="row justify-content-between ml-4">
+                  <div className="col-7 text-light">
+                    <h4>Big boss Burger</h4>
+                  </div>
+                  <div className="col-5">
+                    <ReactStars {...rating} />
+                  </div>
                 </div>
+                <p className="mt-4 ml-5 text-light">$450/piece</p>
+                <div className="row mt-5 justify-content-between ml-5">
+                  <div className="col-7">
+                    <div className="row text-center">
+                      <div className="col-md-3 border border-secondary text-light">
+                        -
+                      </div>
+                      <div className="col-md-3 border border-secondary text-light">
+                        1
+                      </div>
+                      <div className="col-md-3 border border-secondary text-light">
+                        +
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-5">
+                    <h4 className="text-center text-danger">$129</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br />
+            {/* select variation work */}
+            <h2 className="text-light">Select Variation</h2>
+            <br />
+            <div className="row justify-content-between">
+              <div className="form-check col-5 ml-3">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios21"
+                  value="+USD 7.00"
+                  onChange={(e) => {
+                    setRadio(e.target.value);
+                  }}
+                />
+                <label
+                  className="form-check-label text-light"
+                  for="exampleRadios21"
+                >
+                  Large Size (24'')
+                </label>
+              </div>
+              <div className="col-4 text-right">
+                <label className="text-light" name="usd1">
+                  +USD 7.00
+                </label>
+              </div>
+            </div>
+            <br />
 
-                
-                    
-                {/* </div> */}
-                
-                
-        </form>
-    );
+            <div className="row justify-content-between">
+              <div className="form-check col-5 ml-3">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios22"
+                  value="+USD 7.00"
+                  onChange={(e) => {
+                    setRadio(e.target.value);
+                  }}
+                />
+                <label
+                  className="form-check-label text-light"
+                  for="exampleRadios22"
+                >
+                  Medium Size (12'')
+                </label>
+              </div>
+              <div className="col-4 text-right">
+                <label className="text-light">+USD 7.00</label>
+              </div>
+            </div>
+            {/* abar add korbo */}
+            <br />
+
+            <div className="row justify-content-between">
+              <div className="form-check col-5 ml-3">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios23"
+                  value="+USD 7.00"
+                  onChange={(e) => {
+                    setRadio(e.target.value);
+                  }}
+                />
+                <label
+                  className="form-check-label text-light"
+                  for="exampleRadios23"
+                >
+                  Small Size (06'')
+                </label>
+              </div>
+              <div className="col-4 text-right">
+                <label className="text-light">+USD 7.00</label>
+              </div>
+            </div>
+            <br />
+
+            {/*You can add extra  */}
+            <h2 className="text-light">You can add extra</h2>
+            <br />
+            <div className="row justify-content-between">
+              <div className="form-check col-5 ml-3">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="sdfsd"
+                  id="exampleRadios69"
+                  //   checked={radio === "+USD 7.00"}
+                  value={"+USD 7.00"}
+                  onChange={handleChange}
+                />
+                <label
+                  className="form-check-label text-light"
+                  for="exampleRadios69"
+                >
+                  Extra salad
+                </label>
+              </div>
+              <div className="col-4 text-right">
+                <label className="text-light">+USD 7.00</label>
+              </div>
+            </div>
+            <br />
+
+            <div className="row justify-content-between">
+              <div className="form-check col-5 ml-3">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="Rcdcdde"
+                  id="exampleRadios62"
+                  //   checked={radio === "+USD 7.00"}
+                  value="+USD 7.00"
+                  onChange={handleChange}
+                />
+                <label
+                  className="form-check-label text-light"
+                  for="exampleRadios62"
+                >
+                  Extra onion
+                </label>
+              </div>
+              <div className="col-4 text-right">
+                <label className="text-light">+USD 7.00</label>
+              </div>
+            </div>
+            <br />
+
+            <div className="row justify-content-between">
+              <div className="form-check col-5 ml-3">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="examp"
+                  id="exampleRadios72"
+                  //   checked={radio === "+USD 7.00"}
+                  value={"+USD 7.00"}
+                  onChange={handleChange}
+                />
+                <label
+                  className="form-check-label text-light"
+                  for="exampleRadios72"
+                >
+                  Drink
+                </label>
+              </div>
+              <div className="col-4 text-right">
+                <label className="text-light">+USD 7.00</label>
+              </div>
+            </div>
+            <br />
+            {/* special instruction text area*/}
+            <form>
+              <div class="form-group">
+                <h2 for="exampleFormControlTextarea1" className="text-light">
+                  Special Instruction
+                </h2>
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  placeholder="write here"
+                  rows="3"
+                  name="textArea1"
+                  value={textArea}
+                  onChange={(e) => onTextChange(e)}
+                ></textarea>
+              </div>
+            </form>
+            <br />
+            {/* DropDown */}
+            <Form>
+              <Form.Group controlId="exampleForm.SelectCustomSizeSm">
+                <h2 className="text-light">If this product is not available</h2>
+                <Form.Control
+                  as="select"
+                  size="sm"
+                  custom
+                  onChange={(e) => {
+                    setDropdown(e.target.value);
+                  }}
+                >
+                  <option value="Select one">Select one</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </Form.Control>
+              </Form.Group>
+            </Form>
+            <br />
+            <button
+              type="submit"
+              className="btn btn-success btn-block rounded-pill mt-2 checkoutBtn"
+            >
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* </div> */}
+    </form>
+  );
 };
 
 export default ProductPopUp;
