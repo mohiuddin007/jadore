74bd63df764746ceb9f67e87eca11ec2092a98f6
Junaid-md-ullah <junaid00.ju@gmail.com>
2021-01-13 16:16:10 +0600

    language added

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -6,9 +6,19 @@ import userIcon from '../../../images/HomePageResources/User Panel.svg';
 import cartIcon from '../../../images/HomePageResources/Group 108.svg';
 import { Link } from 'react-router-dom';
 import { ProductContext } from '../../../App';
-
+import ScriptTag from 'react-script-tag';
 const Navbar = () => {
     const [productsInfo, SetProductsInfo] = useContext(ProductContext);
+
+    const Demo = props => (
+        <ScriptTag type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" />
+        )
+
+        function googleTranslateElementInit() {
+            new Demo.translate.TranslateElement({pageLanguage: 'fr'}, 'google_translate_element');
+          }
+
+   
     return (
         <nav className="navbar navbar-expand-lg sticky-top navbar-dark navbarBg text-center">
             <Link to="/">
@@ -21,7 +31,7 @@ const Navbar = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mx-auto">
                     <li className="nav-item active mr-md-5 mr-0 mx-5 mx-md-0">
-                        <a className="nav-link" href="/products">All Items</a>
+                        <Link className="nav-link" to="/products">All Items</Link>
                     </li>
                     <li className="nav-item mx-5">
                         <a className="nav-link" href="#">Most Popular</a>
@@ -32,14 +42,15 @@ const Navbar = () => {
                         </Link>
                     </li>
                     <li className="nav-item dropdown mx-5">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {/* <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Language
-                                 </a>
-                        <div className="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+                                 </a> */}
+                                 <div id="google_translate_element"></div>
+                        {/* <div className="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
                             <a className="dropdown-item text-white" href="#">France</a>
                             <a className="dropdown-item text-white" href="#">English</a>
 
-                        </div>
+                        </div> */}
                     </li>
                     <li className="nav-item mr-5">
                         <span><img src={loveIcon} className='ml-5 icon' alt="" /></span>
